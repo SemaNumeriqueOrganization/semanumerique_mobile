afb2fe6916e8cedcb733f0340281b7c9acaafa0f
Marcott <kasangandjomarcott47@gmail.com>
2024-05-08 14:38:12 +0200

    Agrangement

---
+++ lib/data/model/model.dart
@@ -0,0 +1,21 @@
+class VacancyModel {
+  final String? title;
+  final String? subTitle;
+  final String? minSalary;
+  final String? maxSalary;
+  final String? lieux;
+  final String? dateAdd;
+  final String? travail;
+  final String? image;
+
+  VacancyModel({
+    this.title,
+    this.subTitle,
+    this.minSalary,
+    this.maxSalary,
+    this.lieux,
+    this.dateAdd,
+    this.travail,
+    this.image,
+  });
+}

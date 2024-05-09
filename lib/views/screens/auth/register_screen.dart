13f60bc10d9b85cdad039462cffe810f0816e3e3
Marcott <kasangandjomarcott47@gmail.com>
2024-05-09 09:55:13 +0200

    Reg

--- lib/views/screens/auth/register_screen.dart
+++ lib/views/screens/auth/register_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:semanumerique_mobile/core/core.dart';
+import 'package:semanumerique_mobile/import.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          "Register Screen",
-          style: TextStyle(color: MkasColor.primaryColor),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Register Screen",
+              style: TextStyle(color: MkasColor.primaryColor),
+            ),
+            TextButton(
+              onPressed: () => Get.back(),
+              child: const Text("Login"),
+            ),
+          ],
         ),
+        
       ),
     );
   }

facfb19fc9fb2360a3a93ffe0c4069a2eda84fca
Juniorkaniki <juniorkaniki00@gmail.com>
2024-05-10 15:38:45 +0200

    fin login

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -42,12 +42,12 @@ class LoginScreen extends StatelessWidget {
               ),
               height: double.infinity,
               width: double.infinity,
-              child: const Padding(
+              child: Padding(
                 padding: EdgeInsets.only(left: 20.0, right: 20.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                     TextField(
+                    const TextField(
                       decoration: InputDecoration(
                           suffixIcon: Icon(Icons.check, color: Colors.grey),
                           label: Text(
@@ -58,7 +58,7 @@ class LoginScreen extends StatelessWidget {
                             ),
                           )),
                     ),
-                    TextField(
+                    const TextField(
                       decoration: InputDecoration(
                           suffixIcon:
                               Icon(Icons.visibility_off, color: Colors.grey),
@@ -70,7 +70,70 @@ class LoginScreen extends StatelessWidget {
                             ),
                           )),
                     ),
-                  SizedBox(height: 20,),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'Forgert Password?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                          color: Color(0xff2A1639),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 70,
+                    ),
+                    Container(
+                      height: 50,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        gradient: const LinearGradient(colors: [
+                          Color(0xffB81736),
+                          Color(0xff2A1639),
+                        ]),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'SIGN IN',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 150,
+                    ),
+                    const Align(
+                      alignment: Alignment.bottomRight,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Don't have account ?",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
